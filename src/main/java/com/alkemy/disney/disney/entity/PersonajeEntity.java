28f9bd8ce6557c2e0139601447cdb665a1ddda44
nicolaspucci1989 <nicolaspucci1989@gmail.com>
2022-01-14 21:10:40 -0300

    Entrega 01 - Entidades (#1)
    Modelado de entidades

---
+++ src/main/java/com/alkemy/disney/disney/entity/PersonajeEntity.java
@@ -0,0 +1,39 @@
+package com.alkemy.disney.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "personaje")
+@Getter
+@Setter
+public class PersonajeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Long id;
+
+  private String imagen;
+
+  private String nombre;
+
+  private Integer edad;
+
+  private Float peso;
+
+  private String historia;
+
+  @ManyToMany(mappedBy = "personajes", cascade = CascadeType.ALL)
+  private Set<PeliculaEntity> peliculas = new HashSet<>();
+
+  public void agregarPelicula(PeliculaEntity pelicula) {
+    this.peliculas.add(pelicula);
+  }
+
+  public void eliminarPelicula(PeliculaEntity pelicula) {
+    this.peliculas.remove(pelicula);
+  }
+}
