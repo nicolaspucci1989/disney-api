5464a94ea2c9498fb2fb5954f79ba20f430574c7
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-01-29 23:39:03 -0300

    agregando sendgrid

--- src/main/java/com/alkemy/disney/disney/auth/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.disney.disney.auth.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailService {
-  public void sendWelcomeEmailTo(String username) {
-  }
-}
--- src/main/java/com/alkemy/disney/disney/auth/service/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/disney/disney/auth/service/UserDetailsCustomService.java
@@ -3,6 +3,7 @@ package com.alkemy.disney.disney.auth.service;
 import com.alkemy.disney.disney.auth.dto.UserDTO;
 import com.alkemy.disney.disney.auth.entity.AppUser;
 import com.alkemy.disney.disney.auth.repository.UserRepository;
+import com.alkemy.disney.disney.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
