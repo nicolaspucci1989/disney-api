f286e9cf7a17edd9817a3dfa4f2e090ebeb0263b
nicolaspucci1989 <nicolaspucci1989@gmail.com>
2022-02-16 00:54:44 -0300

    Test (#11)
    * add junit
    
    * add smoke test
    
    * add invalid movie create test

--- src/main/java/com/alkemy/disney/disney/dto/MovieDTO.java
+++ src/main/java/com/alkemy/disney/disney/dto/MovieDTO.java
@@ -1,8 +1,7 @@
 package com.alkemy.disney.disney.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotBlank;
@@ -12,6 +11,9 @@ import java.util.List;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MovieDTO {
   private Long id;
 
---
+++ src/test/java/com/alkemy/disney/disney/MovieControllerTest.java
@@ -0,0 +1,60 @@
+package com.alkemy.disney.disney;
+
+import com.alkemy.disney.disney.dto.MovieDTO;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@DisplayName("Given a movie controller")
+@WithMockUser
+public class MovieControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Transactional
+  @Test
+  @DisplayName("should return 400 when creating a invalid movie")
+  public void createInvalidMovie() throws Exception {
+    MovieDTO movieDTO = MovieDTO.builder()
+        .image("/img/image.jpg")
+        .creationDate(LocalDate.of(2000,1,1))
+        .rating(3)
+        .build();
+
+    mockMvc.perform(
+        post("/movies")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(getMapper().writeValueAsString(movieDTO))
+    )
+        .andExpect(status().isBadRequest());
+  }
+
+  private static ObjectMapper getMapper() {
+    var mapper = new ObjectMapper();
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    mapper.registerModule(new JavaTimeModule());
+    return mapper;
+  }
+}
