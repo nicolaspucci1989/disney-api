5720c6054f82a3fbe7031102666b83cf75c1a857
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-02-15 23:53:17 -0300

    add smoke test

--- src/test/java/com/alkemy/disney/disney/DisneyApplicationTests.java
+++ src/test/java/com/alkemy/disney/disney/DisneyApplicationTests.java
@@ -1,13 +1,18 @@
 package com.alkemy.disney.disney;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@DisplayName("Smoke test")
 @SpringBootTest
 class DisneyApplicationTests {
 
   @Test
-  void contextLoads() {
+  @DisplayName("should pass")
+  void smokeTest() {
+    Assertions.assertTrue(true);
   }
 
 }
