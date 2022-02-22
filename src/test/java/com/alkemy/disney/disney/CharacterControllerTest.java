de8ee6cca3327777185ff21366ecc9c52e8382d5
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-02-21 22:11:45 -0300

    test: update character

--- src/test/java/com/alkemy/disney/disney/CharacterControllerTest.java
+++ src/test/java/com/alkemy/disney/disney/CharacterControllerTest.java
@@ -1,6 +1,8 @@
 package com.alkemy.disney.disney;
 
 import com.alkemy.disney.disney.dto.CharacterDTO;
+import com.alkemy.disney.disney.service.CharacterService;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
+import static com.alkemy.disney.disney.TestHelper.getCharacterDTO;
 import static com.alkemy.disney.disney.TestHelper.getMapper;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,17 +30,15 @@ public class CharacterControllerTest {
 
   @Autowired
   MockMvc mockMvc;
+  @Autowired
+  CharacterService characterService;
 
   @Transactional
   @Test
   @DisplayName("should return 400 when creating an invalid character")
   public void createInvalidCharacter() throws Exception {
-    CharacterDTO characterDTO = CharacterDTO.builder()
-        .image("/img/character.jpg")
-        .age(40)
-        .history("Some history")
-        .name("Name")
-        .build();
+    CharacterDTO characterDTO = getCharacterDTO();
+    characterDTO.setName("");
 
     mockMvc.perform(
             post("/characters")
@@ -50,13 +52,7 @@ public class CharacterControllerTest {
   @Test
   @DisplayName("should return 201 when creating a valid character")
   public void createValidCharacter() throws Exception {
-    CharacterDTO characterDTO = CharacterDTO.builder()
-        .image("/img/character.jpg")
-        .name("Character Name")
-        .age(30)
-        .weight(90f)
-        .history("Some history")
-        .build();
+    CharacterDTO characterDTO = getCharacterDTO();
 
     mockMvc.perform(
             post("/characters")
@@ -64,5 +60,33 @@ public class CharacterControllerTest {
                 .content(getMapper().writeValueAsString(characterDTO))
         )
         .andExpect(status().isCreated());
+  }
+
+  @Transactional
+  @Test
+  @DisplayName("should return 200 when updating a character")
+  public void updateCharacter() throws Exception {
+    CharacterDTO characterDTO = getCharacterDTO();
+    String originalName = "Original Name";
+    String updatedName = "Updated character name";
+    characterDTO.setName(originalName);
+    characterService.save(characterDTO);
+
+
+    mockMvc.perform(
+            get("/characters/1")
+        )
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name", Is.is(originalName)));
+
+    characterDTO.setName(updatedName);
+
+    mockMvc.perform(
+            put("/characters/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(getMapper().writeValueAsString(characterDTO))
+        )
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name", Is.is(updatedName)));
   }
 }
