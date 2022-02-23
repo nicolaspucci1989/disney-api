059e6db4a564b744955c75666cfa9dcfb6e3c417
nicolaspucci1989 <nicolaspucci1989@gmail.com>
2022-02-23 11:21:48 -0300

    Add tests (#13)
    * refactor: move getMovieDTO method to helper class
    
    * refactor: move getCharacterDTO method to helper class
    
    * test: update character
    
    * test: refactor movie tests
    
    * chore: format code

--- src/test/java/com/alkemy/disney/disney/TestHelper.java
+++ src/test/java/com/alkemy/disney/disney/TestHelper.java
@@ -1,11 +1,38 @@
 package com.alkemy.disney.disney;
 
+import com.alkemy.disney.disney.dto.CharacterDTO;
+import com.alkemy.disney.disney.dto.MovieDTO;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class TestHelper {
+  public static MovieDTO getMovieDTO() {
+    return MovieDTO.builder()
+        .characters(new ArrayList<>())
+        .title("Title")
+        .genreId(1L)
+        .creationDate(LocalDate.of(2000, 1, 1))
+        .image("/img/movie.jpg")
+        .rating(5)
+        .build();
+  }
+
+  public static CharacterDTO getCharacterDTO() {
+    return CharacterDTO.builder()
+        .image("/img/character.jpg")
+        .name("Character")
+        .age(30)
+        .weight(90f)
+        .history("History")
+        .movies(new ArrayList<>())
+        .build();
+  }
+
   public static ObjectMapper getMapper() {
     var mapper = new ObjectMapper();
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
